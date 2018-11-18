e215aed824092c119396976a4d9c2e37bbc2c3d2
Amber Lee <amblee2009@gmail.com>
2018-11-17 16:46:34 -0800

    update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(300, 300, Phaser.AUTO, 'game');
+var game = new Phaser.Game(500, 500, Phaser.AUTO, 'game');
 
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
